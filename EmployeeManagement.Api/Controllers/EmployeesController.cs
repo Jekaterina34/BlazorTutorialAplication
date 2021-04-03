74f161d4335a68bab2142f50231344200860a22d
Jekaterina34 <80347894+Jekaterina34@users.noreply.github.com>
2021-04-03 17:27:20 +0300

    Inti(17video)

--- EmployeeManagement.Api/Controllers/EmployeesController.cs
+++ EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -60,7 +60,17 @@ namespace EmployeeManagement.Api.Controllers
             try
             {
                 if (employee == null)
+                {
                     return BadRequest();
+                }
+
+                var emp = employeeRepository.GetEmployeeByEmail(employee.Email);
+
+                if(emp !=null)
+                {
+                    ModelState.AddModelError("email", "Employee email already in use");
+                    return BadRequest(ModelState);
+                }
 
                 var createdEmployee = await employeeRepository.AddEmployee(employee);
 
