2426b0e5b5a8e6237f5ea23ecfc4f5ba709fde44
Jekaterina34 <80347894+Jekaterina34@users.noreply.github.com>
2021-04-10 16:35:09 +0300

    Video54

--- EmployeeManagement.Web/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ EmployeeManagement.Web/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -104,7 +104,7 @@ using EmployeeManagement.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 33 "C:\DEV\Blazor Tutorial\EmployeeManagement.Web\Shared\NavMenu.razor"
+#line 38 "C:\DEV\Blazor Tutorial\EmployeeManagement.Web\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
@@ -116,7 +116,7 @@ using EmployeeManagement.Models;
 #line hidden
 #nullable disable
 #nullable restore
-#line 39 "C:\DEV\Blazor Tutorial\EmployeeManagement.Web\Shared\NavMenu.razor"
+#line 44 "C:\DEV\Blazor Tutorial\EmployeeManagement.Web\Shared\NavMenu.razor"
 
     private void HandleClick()
     {
